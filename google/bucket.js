1231a50236a7cc693fc55c35b5b04e78868afa1c
J. E. Minick <james.minick@prodigy.net>
2021-06-28 09:27:46 -0700

    Still working on the Google key issues...

--- google/bucket.js
+++ google/bucket.js
@@ -1,16 +1,28 @@
+require("dotenv").config()
+
 const Cloud = require( '@google-cloud/storage' )
 const path =  require( 'path' )
 const serviceKey = path.join(__dirname,'./keys.json');
 
 const { Storage } = Cloud
 
+// projectId: 'storage'
 const storage = new Storage({
-    keyFilename: serviceKey,
-    projectId: 'storage'
+  projectId: process.env.GCLOUD_PROJECT, 
+  keyFilename: serviceKey
 })
 
-const bucketName = 'cpo-ppvt-bucket'
+// projectid: process.env.GCLOUD_PROJECT, 
+// const storage = new Storage({
+//   projectId: 'storage', 
+//   credentials: {
+//     client_email: process.env.GCLOUD_CLIENT_EMAIL, 
+//     private_key: process.env.GCLOUD_PRIVATE_KEY
+//   } 
+// });
 
-const bucket = storage.bucket(bucketName)
+// const bucketName = 'cpo-ppvt-bucket'
+
+const bucket = storage.bucket(process.env.GCS_BUCKET)
 
 module.exports = bucket
