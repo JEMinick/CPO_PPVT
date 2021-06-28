8f3c7c4b54a86562863569877411749622eceb2d
J. E. Minick <james.minick@prodigy.net>
2021-06-28 15:29:01 -0700

    Adding .env options to the google bucket...

--- google/bucket.js
+++ google/bucket.js
@@ -2,13 +2,79 @@ require("dotenv").config()
 
 const Cloud = require( '@google-cloud/storage' )
 const path =  require( 'path' )
-const serviceKey = path.join(__dirname,'./keys.json');
+let serviceKey = path.join(__dirname,'./keys.json');
+let outputFile = path.join(__dirname,'./keys2.json');
 
 const { Storage } = Cloud
 
+let bError = false;
+const fs = require('fs');
+
+let sLine="";
+var sEnvKey = `${process.env.GCS_PROJECT_KEY}`;
+var aEnvKey = sEnvKey.split("\n");
+var sNewEnvKey = aEnvKey[0]+'\\n';
+for ( var i=1; i < aEnvKey.length; i++ ) {
+  sLine = aEnvKey[i];
+  sNewEnvKey = sNewEnvKey + sLine + ((i < aEnvKey.length-1) ? '\\n' : '');
+}
+// console.log( `New EnvKey:` );
+// console.log( sNewEnvKey );
+
+const sKeysJSON =`
+{
+  "type": "service_account",
+  "project_id": "${process.env.GCS_PROJECT_ID}",
+  "private_key_id": "${process.env.GCS_PROJECT_KEY_ID}",
+  "private_key": "${sNewEnvKey}",
+  "client_email": "${process.env.GCS_CLIENT_EMAIL}",
+  "client_id": "${process.env.GCS_CLIENT_ID}",
+  "auth_uri": "${process.env.GCS_AUTH_URL}",
+  "token_uri": "${process.env.GCS_TOKEN_URL}",
+  "auth_provider_x509_cert_url": "${process.env.GCS_AUTH_PROVIDER_X509_CERT_URL}",
+  "client_x509_cert_url": "${process.env.GCS_CLIENT_X509_CERT_URL}"
+}
+`;
+
+
+// var stream = fs.createWriteStream("my_file.json");
+// stream.once('open', function(fd) {
+//   stream.write(`{\n`);
+//   stream.write(`  "type": "service_account",\n`);
+//   stream.write(`  "project_id": "${process.env.GCS_PROJECT_ID}",\n`);
+//   stream.write(`  "private_key_id": "${process.env.GCS_PROJECT_KEY_ID}",\n`);
+//   stream.write(`  "private_key": "${sNewEnvKey}",\n`);
+//   stream.write(`  "client_email": "${process.env.GCS_CLIENT_EMAIL}",\n`);
+//   stream.write(`  "client_id": "${process.env.GCS_CLIENT_ID}",\n`);
+//   stream.write(`  "auth_uri": "${process.env.GCS_AUTH_URL}",\n`);
+//   stream.write(`  "token_uri": "${process.env.GCS_TOKEN_URL}",\n`);
+//   stream.write(`  "auth_provider_x509_cert_url": "${process.env.GCS_AUTH_PROVIDER_X509_CERT_URL}",\n`);
+//   stream.write(`  "client_x509_cert_url": "${process.env.GCS_CLIENT_X509_CERT_URL}"\n`);
+//   stream.write(`}\n`);
+//   stream.end();
+// });
+
+fs.writeFile( outputFile, sKeysJSON, (error) => {
+  if ( error ) {
+      bError = true;
+      console.error(error);
+  } else {
+    // if ( bDebugging )
+    //   console.log(`\nFILE CONTENTS:`)
+  }
+});
+if ( !bError ) {
+  console.log( `Created file: ${outputFile}...` );
+  serviceKey = outputFile;
+}
+else {
+  console.log( `Unable to create file: ${outputFile}...` );
+}
+
+
 // projectId: 'storage'
 const storage = new Storage({
-  projectId: process.env.GCLOUD_PROJECT, 
+  projectId: process.env.GCS_PROJECT_ID, 
   keyFilename: serviceKey
 })
 
