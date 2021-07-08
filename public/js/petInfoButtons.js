ecd2eb4c99f0ec18bdfe756894f5661d41647f48
J. E. Minick <james.minick@prodigy.net>
2021-07-08 08:51:13 -0700

    Modied addVaccine method to accomodate uniqueness in order to permit multiple vaccine records.

--- public/js/petInfoButtons.js
+++ public/js/petInfoButtons.js
@@ -1,18 +1,12 @@
 
 // button to ADD a new vaccine...
-const userAddVaccine = function () {
-  const iPetID = document.querySelector('input[name="pet-id"]').value;
-  console.log( `Triggering ADD VACCINE for pet# [${iPetID}]` );
-  document.location.replace(`/add-vaccine/${iPetID}`);
-};
-
-// button to DELETE the pet and its associated vaccines:
-const userDeletePet = function () {
-  const iPetID = document.querySelector('input[name="pet-id"]').value;
-  console.log( `Triggering DELETE PET# [${iPetID}]` );
-  document.location.replace(`/api/pets/del/${iPetID}`);
-  document.location.replace('/');
-};
+const addVaccine = function (event) {
+  if ( event.target.matches(".addvaccinebtn") ) {
+    const iPetID = event.target.getAttribute("data-petid")
+    // console.log( `Triggering ADD VACCINE for pet# [${iPetID}]` )
+    document.location.replace(`/add-vaccine/${iPetID}`)
+  }
+}
 
 const deletePet = async (event) => {
   event.preventDefault();
@@ -34,183 +28,82 @@ const deletePet = async (event) => {
 
   if ( iPetID > 0 ) {
 
-    // if ( pet_photo ) {
+    // We need a full record for the pet from the database in order to perform
+    // delete api requests within the google cloud bucket for any images
+    // the user has uploaded:
 
-    //   console.log( `Uploading image: [${pet_photo}]` );
+    let response = await fetch(`/api/pets/retrieve/${iPetID}`, {
+      method: 'GET'
+    })
 
-    //   let formPhotoImage = new FormData()
-    //   formPhotoImage.append("file", file)
-    //   const response = await fetch('/uploadimages', {
-    //       method: "POST", 
-    //       body: formPhotoImage
-    //   })
-      
-    //   const {data} = await response.json();
+    if ( response.ok ) {
 
-    //   console.log(  `POST (image) response:` );
-    //   console.log( response );
+      let jResponse = await response.json();
+      let file='';
 
-    //   pet_photo = data;
+      // console.log( `pet_photo: [${jResponse.pet_photo}]` );
+      if ( jResponse.pet_photo ) {
+        if ( jResponse.pet_photo.length ) {
+          file = jResponse.pet_photo;
+          // console.log( `Deleting image: [${file}]` );
 
-    //   console.log( `Pet Image File:` );
-    //   console.log( data );
-    // }
-
-    // let pet_license_file;
-    // fieldSel = document.getElementById("upload-license");
-    // if ( fieldSel ) {
-    //   file = fieldSel.files[0];
-    //   if ( file ) {
-    //     pet_license_file = file.name
-    //   }
-    // }
-
-    // if ( pet_license_file ) {
-
-    //   console.log( `Uploading image: [${pet_license_file}]` );
-
-    //   let formLicenseData = new FormData()
-    //   formLicenseData.append("file", file)
-    //   const response = await fetch('/uploadimages', {
-    //       method: "POST", 
-    //       body: formLicenseData
-    //   })
-      
-    //   const {data} = await response.json();
-
-    //   console.log(  `POST (image image) response:` );
-    //   console.log( response );
-
-    //   pet_license_file = data;
-
-    //   console.log( `Pet License File:` );
-    //   console.log( data );
-    // }
-
-    if ( iPetID > 0 ) {
-      let response = await fetch(`/api/pets/retrieve/${iPetID}`, {
-        method: 'GET'
-        // body: JSON.stringify(
-        //     { iPetID,
-        //       petname
-        //     }),
-        // headers: { 'Content-Type': 'application/json' },
-      })
-      // .then(response => response.json())
-      // .then(result => {
-      //   console.log('Success:', result);
-      // })
-      // .catch(error => {
-      //   console.error('Error:', error);
-      // });      
-
-      if ( response.ok ) {
-
-        let jResponse = await response.json();
-        let file='';
-
-        console.log( `RETRIEVED pet info:` );
-        console.log( jResponse );
-
-        console.log( JSON.stringify(jResponse) );
-        console.log( `\nDATA:` );
-        console.log( `pet id: [${jResponse.id}]` );
-
-
-        console.log( `pet_photo: [${jResponse.pet_photo}]` );
-        if ( jResponse.pet_photo ) {
-          if ( jResponse.pet_photo.length ) {
-            file = jResponse.pet_photo;
-            console.log( `Deleting image: [${file}]` );
-
-            let formLicenseData = new FormData()
-            formLicenseData.append("file", file)
-            // let jFileInfo = {
-            //   "file": jResponse.pet_vaccines[i].vaccine_license_file
-            // }
-            const response = await fetch('/uploadimages', {
-                method: "DELETE", 
-                body: formLicenseData
-            })
-            
-            const {data} = await response.json();
-    
-            console.log(  `DELETE (image) response:` );
-            console.log( response );
-          }
-        }  
-
-        console.log( `pet_license_file: [${jResponse.pet_license_file}]` );
-        if ( jResponse.pet_license_file ) {
-          if ( jResponse.pet_license_file.length ) {
-            file = jResponse.pet_license_file;
-            console.log( `Deleting image: [${file}]` );
-
-            let formLicenseData = new FormData()
-            formLicenseData.append("file", file)
-            // let jFileInfo = {
-            //   "file": jResponse.pet_vaccines[i].vaccine_license_file
-            // }
-            const response = await fetch('/uploadimages', {
-                method: "DELETE", 
-                body: formLicenseData
-            })
-            
-            const {data} = await response.json();
-    
-            console.log(  `DELETE (image) response:` );
-            console.log( response );
-          }
+          let formLicenseData = new FormData()
+          formLicenseData.append("file", file)
+          const response = await fetch('/uploadimages', {
+              method: "DELETE", 
+              body: formLicenseData
+          })
+          
+          const {data} = await response.json();
         }
+      }  
 
-        let iTotalVaccines=jResponse.pet_vaccines.length
-        console.log( `Total # of vaccines for this pet: ${iTotalVaccines}` );
+      console.log( `pet_license_file: [${jResponse.pet_license_file}]` );
+      if ( jResponse.pet_license_file ) {
+        if ( jResponse.pet_license_file.length ) {
+          file = jResponse.pet_license_file;
+          // console.log( `Deleting image: [${file}]` );
 
-        for( var i=0; ( i < iTotalVaccines ); i++ ) {
+          let formLicenseData = new FormData()
+          formLicenseData.append("file", file)
+          const response = await fetch('/uploadimages', {
+              method: "DELETE", 
+              body: formLicenseData
+          })
 
-          console.log( `${i}: ${jResponse.pet_vaccines[i].vaccine_license_file}` );
-
-          if ( jResponse.pet_vaccines[i].vaccine_license_file ) {
-            if ( jResponse.pet_vaccines[i].vaccine_license_file.length ) {
-              file = jResponse.pet_vaccines[i].vaccine_license_file;
-              console.log( `Deleting image: [${file}]` );
-
-              let formLicenseData = new FormData()
-              formLicenseData.append("file", file)
-              // let jFileInfo = {
-              //   "file": jResponse.pet_vaccines[i].vaccine_license_file
-              // }
-              const response = await fetch('/uploadimages', {
-                  method: "DELETE", 
-                  body: formLicenseData
-              })
-              
-              const {data} = await response.json();
-      
-              console.log(  `DELETE (image) response:` );
-              console.log( response );
-      
-              pet_license_file = data;
-      
-              console.log( `Pet License File:` );
-              console.log( data );
-            }
-          }
-  
+          const {data} = await response.json();
         }
-        console.log( `\n` );
-      } else {
-        alert(response.statusText);
       }
-        
-      // console.log( `Attempting to delete pet: [ID:${iPetID}]` );
+
+      let iTotalVaccines=jResponse.pet_vaccines.length
+
+      for( var i=0; ( i < iTotalVaccines ); i++ ) {
+        // console.log( `${i}: ${jResponse.pet_vaccines[i].vaccine_license_file}` );
+        if ( jResponse.pet_vaccines[i].vaccine_license_file ) {
+          if ( jResponse.pet_vaccines[i].vaccine_license_file.length ) {
+            file = jResponse.pet_vaccines[i].vaccine_license_file;
+            // console.log( `Deleting image: [${file}]` );
+            let formLicenseData = new FormData()
+            formLicenseData.append("file", file)
+            const response = await fetch('/uploadimages', {
+                method: "DELETE", 
+                body: formLicenseData
+            })
+            const {data} = await response.json();
+            pet_license_file = data;
+          }
+        }
+      } // endFor
+
+    } else {
+      alert(response.statusText);
     }
+      
+  }
 
-    // console.log(
-    //     { iPetID,
-    //       petname
-    //     });
-
+  // After doing the Google Cloud Bucket clean-up,
+  // perform the cascaded delete within the database:
+  if ( iPetID > 0 ) {
     let response = await fetch(`/api/pets/${iPetID}`, {
         method: 'DELETE',
         body: JSON.stringify(
@@ -225,8 +118,8 @@ const deletePet = async (event) => {
     } else {
       alert(response.statusText);
     }
-
   }
+
 };
 
 document
@@ -234,5 +127,5 @@ document
   .addEventListener('click', deletePet );
 
 document
-  .querySelector('#addvaccinebtn')
-  .addEventListener('click', userAddVaccine );
+  .querySelector('#petinfocontainer')
+  .addEventListener('click', addVaccine );
