fb97aff3dc8612d35092fe59010ab771c28aecae
J. E. Minick <james.minick@prodigy.net>
2021-07-08 08:52:42 -0700

    General cleanup: removing console.log's and unused code.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,11 +54,8 @@ router.get('/', withAuth, (req, res) => {
   })
   .then(dbPetInfo => {
     const petInfo = dbPetInfo.map(subject => subject.get({ plain: true }));
-      // console.log( petInfo );
       if ( !req.session.loggedIn )
         req.session.loggedIn = false;
-      // res.status(200).json(petInfo);
-      console.log( JSON.stringify(petInfo) );
       res.render('homepage', { petInfo, loggedIn: req.session.loggedIn });    
   })
   .catch(err => {
@@ -98,6 +95,9 @@ router.get( '/add-pet', withAuth, (req, res) => {
   res.render( 'edit-pet', {petInfo, loggedIn: req.session.loggedIn} );
 });
 
+// --------------------
+// VACCINE model:
+// --------------------
 // id
 // vaccine_name
 // date_of_vaccine
@@ -107,6 +107,7 @@ router.get( '/add-pet', withAuth, (req, res) => {
 // user_id
 // pet_id
 // date_created
+// --------------------
 
 router.get( '/add-vaccine/:id', withAuth, (req, res) => {
   console.log( `Render 'new-vaccine'...` );
@@ -121,99 +122,8 @@ router.get( '/add-vaccine/:id', withAuth, (req, res) => {
   vaccineInfo.vaccine_license_local = '';
   vaccineInfo.user_id = req.session.user_id;
   vaccineInfo.pet_id = req.params.id;
-  console.log( `INITIATING NEW VACCINE JSON:` );
-  console.log( vaccineInfo );
+  // console.log( `INITIATING NEW VACCINE JSON:` );
   res.render( 'edit-vaccine', {vaccineInfo, loggedIn: req.session.loggedIn} );
 });
-
-// =============================================================================================
-// router.get('/signup', (req, res) => {
-//   res.render('signup');
-// });
-
-// =============================================================================================
-// router.get('/blogpost/:id', (req, res) => {
-//   BlogPost.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     attributes: [
-//       'id',
-//       'subject',
-//       'description',
-//       'date_created'
-//     ],
-//     include: [
-//       {
-//         model: BlogComment,
-//         attributes: ['id', 'blog_comment', 'post_id', 'user_id', 'date_created'],
-//         include: {
-//           model: User,
-//           attributes: ['name']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['name']
-//       }
-//     ]
-//   })
-//   .then(blogPostInfo => {
-//     if (!blogPostInfo) {
-//       res.status(404).json({ message: 'No blog post found with this id' });
-//       return;
-//     }
-//     const post = blogPostInfo.get({ plain: true });
-//     // console.log(post);
-//     // console.log( `Logged In? [${req.session.loggedIn}] : Rendering: blogPost...` );
-//     res.render('blogPost', { post, loggedIn: req.session.loggedIn });
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.status(500).json(err);
-//   });
-// });
-
-// =============================================================================================
-// router.get('/blogpost-comments', (req, res) => {
-//   BlogPost.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     attributes: [
-//       'id',
-//       'subject',
-//       'description',
-//       'date_created'
-//     ],
-//     include: [
-//       {
-//         model: BlogComment,
-//         attributes: ['id', 'blog_comment', 'post_id', 'user_id', 'date_created'],
-//         include: {
-//           model: User,
-//           attributes: ['name']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['name']
-//       }
-//     ]
-//   })
-//   .then(blogPostInfo => {
-//     if ( !blogPostInfo ) {
-//       res.status(404).json({ message: 'No post found with this id' });
-//       return;
-//     }
-//     const post = blogPostInfo.get({ plain: true });
-//     res.render('blogpostcomments', { post, loggedIn: req.session.loggedIn });
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.status(500).json(err);
-//   });
-
-// });
 
 module.exports = router;
